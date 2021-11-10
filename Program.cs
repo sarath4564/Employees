e01b10d95440b634ee9c2887ddd8b67afafbdbd6
SARATHCHANDRA <sarathchandra76@gmail.com>
2021-11-10 21:43:17 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Employees
+{
+    public class Employees
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public int Salary { get; private set; }
+
+        public static void Main()
+        {
+            Employees employees = GetEmployees();
+            Console.WriteLine(employees);
+        }
+
+        private static Employees GetEmployees()
+        {
+            return NewMethod();
+        }
+
+        private static Employees NewMethod()
+        {
+            return new Employees { Id = 1001, Name = "Ashok", Salary = 98000 };
+        }
+    }
+}
